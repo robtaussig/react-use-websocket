2cfe23916cca7615bbf0b3a8a623350a28204c06
Rob Taussig <robert.taussig@gmail.com>
2019-05-04 02:37:45 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,107 @@
+import { useEffect, useRef, useState, useCallback } from 'react';
+
+const sharedWebSockets = {};
+const subscribers = {};
+
+const attachListeners = (webSocketInstance, url, setLastMessage, options) => {
+  if (options.share) {
+    const removeSubscriber = addSubscriber(webSocketInstance, url, setLastMessage, options);
+
+    return removeSubscriber;
+  } else {
+    webSocketInstance.onmessage = message => {
+      if (options.onMessage) options.onMessage(message);
+      setLastMessage(message);
+    };
+  
+    if (options.onOpen) webSocketInstance.onopen = options.onOpen;
+    if (options.onClose) webSocketInstance.onclose = options.onClose;
+    if (options.onError) webSocketInstance.onerror = options.onError;
+
+    return webSocketInstance.close;
+  }
+};
+
+const createOrJoinSocket = (webSocketRef, url, options) => {
+  if (options.share) {
+    if (sharedWebSockets[url] === undefined) {
+      sharedWebSockets[url] = new WebSocket(url);
+    }
+    webSocketRef.current = sharedWebSockets[url];
+  } else {
+    webSocketRef.current = new WebSocket(url);
+  }
+};
+
+const addSubscriber = (webSocketInstance, url, setLastMessage, options = {}) => {
+  if (subscribers[url] === undefined) {
+    subscribers[url] = [];
+
+    webSocketInstance.onmessage = message => {
+      subscribers[url].forEach(subscriber => {
+        subscriber.setLastMessage(message);
+        if (subscriber.options.onMessage) {
+          subscriber.options.onMessage(message);
+        }
+      });
+    };
+
+    webSocketInstance.onclose = event => {
+      subscribers[url].forEach(subscriber => {
+        if (subscriber.options.onClose) {
+          subscriber.options.onClose(event);
+        }
+      });
+
+      subscribers[url] = undefined;
+      sharedWebSockets[url] = undefined;
+    };
+
+    webSocketInstance.onerror = error => {
+      subscribers[url].forEach(subscriber => {
+        if (subscriber.options.onError) {
+          subscriber.options.onError(error);
+        }
+      });
+    };
+  }
+
+  const subscriber = {
+    setLastMessage,
+    options,
+  };
+  subscribers[url].push(subscriber);
+
+  return () => {
+    const index = subscribers[url].indexOf(subscriber);
+    if (index !== -1) {
+      if (subscribers[url].length === 1) {
+        webSocketInstance.close();
+      }
+      subscribers[url].splice(index, 1);
+    } else {
+      throw new Error('A subscriber that is no longer registered has attempted to unsubscribe');
+    }
+  };
+};
+
+export const useWebSocket = (url, options = {}) => {
+  const webSocketRef = useRef(null);
+  const [ lastMessage, setLastMessage ] = useState(null);
+
+  const sendMessage = useCallback(message => {
+    webSocketRef.current && webSocketRef.current.send(message);
+  }, []);
+
+  useEffect(() => {
+    createOrJoinSocket(webSocketRef, url, options);
+
+    const removeListeners = attachListeners(webSocketRef.current, url, setLastMessage, options);
+
+    return removeListeners;
+  }, [url, options]);
+
+  return [ sendMessage, lastMessage ];
+};
+
+export default useWebSocket;
