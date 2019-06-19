bf5e78f7c4cc4785b9f7c1bdf74eb599db85de2c
Rob Taussig <robert.taussig@gmail.com>
2019-06-19 19:49:36 -0400

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for react-use-websocket
+// Project: useWebSocket
+// Definitions by: Robert Taussig <robtaussig.com>
+
+export interface Options {
+  fromSocketIO?: boolean;
+  onMessage?(message: MessageEvent): void;
+  onClose?(event: CloseEvent): void;
+  onError?(error: ErrorEvent): void;
+  onOpen?(event: Event): void;
+  share?: boolean;
+}
+
+enum ReadyState {
+  READY_STATE_CONNECTING = 0,
+  READY_STATE_OPEN = 1,
+  READY_STATE_CLOSING = 2,
+  READY_STATE_CLOSED = 3,
+}
+
+export function useWebSocket(url: string, options?: Options): [() => void, MessageEvent, ReadyState];
