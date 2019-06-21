316fcdbab57723f311320433e8412c2aba185fd7
Alan Smithee <ggnore.alan.smithee@gmail.com>
2019-06-21 13:25:18 +0200

    add socket-io hook

--- dist/index.js
+++ dist/index.js
@@ -3,9 +3,17 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "useSocketIO", {
+  enumerable: true,
+  get: function get() {
+    return _useSocketIo.useSocketIO;
+  }
+});
 exports["default"] = void 0;
 
 var _useWebsocket = require("./lib/use-websocket");
+
+var _useSocketIo = require("./lib/use-socket-io");
 
 var _default = _useWebsocket.useWebSocket;
 exports["default"] = _default;
