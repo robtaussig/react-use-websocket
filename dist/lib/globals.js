80bcbd09f7424b6592b2048b04bb089710db6f5b
Rob Taussig <robert.taussig@gmail.com>
2019-07-26 23:39:40 -0400

    Set up typescript and remove dist folder

--- dist/lib/globals.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.sharedWebSockets = void 0;
-var sharedWebSockets = {};
-exports.sharedWebSockets = sharedWebSockets;
