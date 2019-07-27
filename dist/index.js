2814a5466ed98728c22c8c6fd2c4f1527ddaa229
Rob Taussig <robert.taussig@gmail.com>
2019-07-27 14:16:19 -0400

    Remove dist from git

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var use_websocket_1 = require("./lib/use-websocket");
-var use_socket_io_1 = require("./lib/use-socket-io");
-exports.useSocketIO = use_socket_io_1.useSocketIO;
-exports.default = use_websocket_1.useWebSocket;
-//# sourceMappingURL=index.js.map
--- dist/lib/globals.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sharedWebSockets = {};
-//# sourceMappingURL=globals.js.map
