6a7d89b79753cb831ea9be9a607c5e7ff46ab815
Rob Taussig <robert.taussig@gmail.com>
2019-12-08 02:50:16 -0500

    Improve typing of WebSocket proxy

--- src/lib/proxy.ts
+++ src/lib/proxy.ts
@@ -1,7 +1,15 @@
-export const websocketWrapper = (webSocket: WebSocket): Proxy<WebSocket> => {
+type IfEquals<X, Y, A=X, B=never> =
+  (<T>() => T extends X ? 1 : 2) extends
+  (<T>() => T extends Y ? 1 : 2) ? A : B;
 
-  return new Proxy(webSocket, {
-    get: (obj, key) => {
+type WritableKeys<T> = {
+  [P in keyof T]-?: IfEquals<{ [Q in P]: T[P] }, { -readonly [Q in P]: T[P] }, P>
+}[keyof T];
+
+export const websocketWrapper = (webSocket: WebSocket): WebSocket => {
+
+  return new Proxy<WebSocket>(webSocket, {
+    get: (obj, key: keyof WebSocket) => {
       const val = obj[key];
       if (typeof val === 'function') {
         console.error('Calling methods directly on the websocket is not supported at this moment. You must use the methods returned by useWebSocket.');
@@ -12,11 +20,13 @@ export const websocketWrapper = (webSocket: WebSocket): Proxy<WebSocket> => {
         return val;
       }
     },
-    set: (obj, key, val) => {
+    set: (obj: WebSocket, key:WritableKeys<WebSocket>, val: any) => {
       if (/^on/.test(key)) {
         console.warn('The websocket\'s event handlers should be defined through the options object passed into useWebSocket.')
+        return false;
       } else {
         obj[key] = val;
+        return true;
       }
     },
   });
