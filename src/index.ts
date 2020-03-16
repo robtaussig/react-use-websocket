5ab3365e20c7ab50607224bb5263eff7a74ce864
Ben Grant <benjamin.j.grant@gmail.com>
2020-03-16 14:03:57 -0700

    Export types SendMessage and Options from src/lib/use-websocket.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import { useWebSocket } from './lib/use-websocket';
+export { useWebSocket as default, SendMessage, Options } from './lib/use-websocket';
 
 export { useSocketIO } from './lib/use-socket-io';
 
 export { ReadyState } from './lib/constants';
-
-export default useWebSocket;
