e45cbdf43849fc40da3e52fefcb97b26bc1a021d
Edward Wilson <edward.wilson@me.com>
2022-10-05 15:42:52 +0100

    Update types.ts
    Change JsonValue type, so it allows arrays.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -84,27 +84,13 @@ Matches a [`class` constructor](https://developer.mozilla.org/en-US/docs/Web/Jav
 export type Constructor<T, Arguments extends unknown[] = any[]> = new(...arguments_: Arguments) => T;
 
 /**
-Matches any valid JSON primitive value.
-@category JSON
-*/
-export type JsonPrimitive = string | number | boolean | null;
-
-/**
-Matches a JSON object.
-This type can be useful to enforce some input to be JSON-compatible or as a super-type to be extended from. Don't use this as a direct return type as the user would have to double-cast it: `jsonObject as unknown as CustomResponse`. Instead, you could extend your CustomResponse type from it to ensure your type only uses JSON-compatible types: `interface CustomResponse extends JsonObject { … }`.
-@category JSON
-*/
-export type JsonObject = {[Key in string]?: JsonValue | JsonPrimitive };
-
-/**
-Matches a JSON array.
-@category JSON
-*/
-export type JsonArray = JsonValue[];
-
-/**
 Matches any valid JSON value.
 @see `Jsonify` if you need to transform a type to one that is assignable to `JsonValue`.
 @category JSON
 */
-export type JsonValue = JsonObject | JsonArray;
+export type JsonValue =
+    | string
+    | number
+    | boolean
+    | { [Key in string]?: JsonValue }
+    | Array<JsonValue>;
